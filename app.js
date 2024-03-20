c7f206ca73961f5a5a0077fdd213c1c887fbd9ed
mairarafiq301 <mairarafiq301@gmail.com>
2024-03-20 14:15:47 -0700

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 "use strict";
 let num1 = 20;
-//num1 += 22 ;
-//console.log(num1) ;
+num1 += 22;
+console.log(num1);
 num1 -= 22;
+console.log(num1);
+num1 *= 22;
+console.log(num1);
+num1 /= 22;
+console.log(num1);
+num1 %= 22;
+console.log(num1);
+num1 **= 22;
 console.log(num1);
--- app.ts
+++ app.ts
@@ -1,8 +1,22 @@
 
 let num1 = 20 ;
-//num1 += 22 ;
-//console.log(num1) ;
+
+
+num1 += 22 ;
+console.log(num1) ;
 
 num1 -= 22
+console.log(num1) ;
+
+num1 *= 22
+console.log(num1) ;
+
+num1 /= 22
+console.log(num1) ;
+
+num1 %= 22
+console.log(num1) ;
+
+num1 **= 22
 console.log(num1) ;
 
